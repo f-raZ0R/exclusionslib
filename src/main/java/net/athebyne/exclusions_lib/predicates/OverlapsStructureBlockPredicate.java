10e0a32e718889c38cabfc094c371d3d1e69ba33
f-raZ0R <MonkeyCityOwner@gmail.com>
2024-09-26 20:52:57 -0400

    0.6: Allowed tags or lists of structures. Refactored code to maybe optimize it (Thanks Apollo!)

--- src/main/java/net/athebyne/exclusions_lib/predicates/OverlapsStructureBlockPredicate.java
+++ src/main/java/net/athebyne/exclusions_lib/predicates/OverlapsStructureBlockPredicate.java
@@ -2,87 +2,82 @@ package net.athebyne.exclusions_lib.predicates;
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryCodecs;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.structure.StructurePiece;
 import net.minecraft.structure.StructureStart;
 import net.minecraft.util.math.*;
 import net.minecraft.world.StructureWorldAccess;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.blockpredicate.BlockPredicate;
 import net.minecraft.world.gen.blockpredicate.BlockPredicateType;
 import net.minecraft.world.gen.structure.Structure;
 import net.athebyne.exclusions_lib.*;
+import org.apache.commons.lang3.mutable.MutableBoolean;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 
 public class OverlapsStructureBlockPredicate implements BlockPredicate {
 
     public static final Codec<OverlapsStructureBlockPredicate> CODEC = RecordCodecBuilder.create((instance) -> instance.group(Vec3i.createOffsetCodec(16).optionalFieldOf("offset", BlockPos.ORIGIN).forGetter((predicate) -> predicate.offset),
-            RegistryKey.createCodec(RegistryKeys.STRUCTURE).optionalFieldOf("structure").forGetter((predicate) -> predicate.structure),
+            RegistryCodecs.entryList(RegistryKeys.STRUCTURE).optionalFieldOf("structures").forGetter((predicate) -> predicate.structures),
             Codec.intRange(0, 32).optionalFieldOf("range", 0).forGetter((predicate) -> predicate.range)).apply(instance, OverlapsStructureBlockPredicate::new));
 
     private final Vec3i offset;
     private final int range;
-    private final Optional<RegistryKey<Structure>> structure;
-    public OverlapsStructureBlockPredicate(Vec3i offset,  Optional<RegistryKey<Structure>> structure, int range) {
-        this.structure = structure;
+    private final Optional<RegistryEntryList<Structure>> structures;
+    public OverlapsStructureBlockPredicate(Vec3i offset,  Optional<RegistryEntryList<Structure>> structures, int range) {
+        this.structures = structures;
         this.offset = offset;
         this.range = range;
     }
-
-    public boolean checkChunkStructureInFeature(Structure targetStruct, ServerWorld serverWorld, BlockPos blockPos, int xChunkOffset, int zChunkOffset)
-    {
-        BlockBox exclusionZone = new BlockBox(blockPos.getX() - this.range, blockPos.getY() - this.range, blockPos.getZ() - this.range,blockPos.getX() + this.range, blockPos.getY() + this.range, blockPos.getZ() + this.range);
-
-        Chunk thisChunk = serverWorld.getChunk(ChunkSectionPos.getSectionCoord(blockPos.getX() + xChunkOffset * 16), ChunkSectionPos.getSectionCoord(blockPos.getZ() + zChunkOffset * 16), ChunkStatus.EMPTY, false);
-
-        if(thisChunk!=null && thisChunk.getStatus().isAtLeast(ChunkStatus.STRUCTURE_REFERENCES))
+    public boolean test(StructureWorldAccess structureWorldAccess, BlockPos blockPos) {
+        StructureAccessor accessor = structureWorldAccess.toServerWorld().getStructureAccessor();
+        Registry<Structure> structureKey = accessor.getRegistryManager().get(RegistryKeys.STRUCTURE);
+        BlockPos blockPosOffset = blockPos.add(this.offset);
+        List<Structure> targetStructs = new ArrayList<>();
+        if(this.structures.isPresent())
         {
-            for(var struct : thisChunk.getStructureReferences().entrySet())
+            for(RegistryEntry<Structure> entry : this.structures.get())
             {
-                if(targetStruct!= null && targetStruct!=struct.getKey())
+                if(entry.getKey().isPresent())
                 {
-                    continue;
-                }
-                for (Long longPos : struct.getValue()) {
-                    ChunkSectionPos chunkSectionPos = ChunkSectionPos.from(new ChunkPos(longPos), serverWorld.getBottomSectionCoord());
-                    StructureStart structureStart = serverWorld.getStructureAccessor().getStructureStart(chunkSectionPos, struct.getKey(), serverWorld.getChunk(chunkSectionPos.getSectionX(), chunkSectionPos.getSectionZ(), ChunkStatus.EMPTY, false));
-                    if (structureStart == null) {
-                        continue;
-                    }
-                    if (structureStart.getBoundingBox().intersects(exclusionZone)) {
-                        return true;
-                    } else {
-                        for (StructurePiece piece : structureStart.getChildren()) {
-                            if (piece.getBoundingBox().intersects(exclusionZone)) {
-                                return true;
-                            }
-                        }
-                    }
+                    targetStructs.add(structureKey.get(entry.getKey().get()));
                 }
             }
         }
-        return false;
-    }
-
-
-    public boolean test(StructureWorldAccess structureWorldAccess, BlockPos blockPos) {
-        ServerWorld serverWorld = structureWorldAccess.toServerWorld();
-        BlockPos blockPosOffset = blockPos.add(this.offset);
-        Structure targetStruct = null;
-        if(this.structure.isPresent())
+        BlockBox exclusionZone = new BlockBox(blockPosOffset.getX() - this.range, blockPosOffset.getY() - this.range, blockPosOffset.getZ() - this.range,blockPosOffset.getX() + this.range, blockPosOffset.getY() + this.range, blockPosOffset.getZ() + this.range);
+        for(var struct : accessor.getStructureReferences(blockPosOffset).entrySet())
         {
-            targetStruct = serverWorld.getStructureAccessor().getRegistryManager().get(RegistryKeys.STRUCTURE).get(this.structure.get());
-        }
-        for (int x = -1; x <= 1; x++) {
-            for (int z = -1; z <= 1; z++) {
-                if(checkChunkStructureInFeature(targetStruct,serverWorld, blockPosOffset, x, z))
-                {
-                    return true;
+            if(!targetStructs.isEmpty() && !targetStructs.contains(struct.getKey()))
+            {
+                continue;
+            }
+            Predicate<StructureStart> predicate = start -> {
+                for (StructurePiece piece : start.getChildren()) {
+                    if (piece.getBoundingBox().intersects(exclusionZone)) {
+                        return true;
+                    }
                 }
+                return false;
+            };
+
+            MutableBoolean overlappingBox = new MutableBoolean(false);
+            accessor.acceptStructureStarts(struct.getKey(), struct.getValue(), (start) -> {
+                if (overlappingBox.isFalse() && predicate.test(start)) {
+                    overlappingBox.setTrue();
+                }
+            });
+
+            if (overlappingBox.isTrue()) {
+                return true;
             }
         }
         return false;
