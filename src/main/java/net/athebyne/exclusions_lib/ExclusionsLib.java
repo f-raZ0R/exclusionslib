b52fcd305fc2da083772faec7b374a6f573673b9
imreallybadatnames <pizzagtav@gmail.com>
2025-01-31 16:16:45 +1100

    random bullshit go

--- src/main/java/net/athebyne/exclusions_lib/ExclusionsLib.java
+++ src/main/java/net/athebyne/exclusions_lib/ExclusionsLib.java
@@ -1,8 +1,8 @@
 package net.athebyne.exclusions_lib;
+
 import net.athebyne.exclusions_lib.predicates.OverlapsStructureBlockPredicate;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
+import net.minecraft.registry.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.blockpredicate.BlockPredicateType;
 import org.slf4j.Logger;
@@ -12,9 +12,9 @@ public class ExclusionsLib implements ModInitializer {
     public static final String MOD_ID = "exclusions_lib";
     public static BlockPredicateType<OverlapsStructureBlockPredicate> OVERLAPS_STRUCTURE;
     public static final Logger LOGGER = LoggerFactory.getLogger("Exclusions Lib");
+
     @Override
     public void onInitialize() {
-        OVERLAPS_STRUCTURE = Registry.register(Registries.BLOCK_PREDICATE_TYPE, new Identifier(MOD_ID, "overlaps_structure"), () -> OverlapsStructureBlockPredicate.CODEC);
-
+        OVERLAPS_STRUCTURE = Registry.register(Registries.BLOCK_PREDICATE_TYPE, Identifier.of(MOD_ID, "overlaps_structure"), () -> OverlapsStructureBlockPredicate.CODEC);
     }
 }
--- src/main/java/net/athebyne/exclusions_lib/mixin/TagGroupLoaderMixin.java
+++ src/main/java/net/athebyne/exclusions_lib/mixin/TagGroupLoaderMixin.java
@@ -1,20 +1,18 @@
 package net.athebyne.exclusions_lib.mixin;
 
 import com.google.common.collect.ImmutableSet;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.athebyne.exclusions_lib.ExclusionsLib;
 import net.athebyne.exclusions_lib.extensions.TagEntryExclusionHolder;
-import net.minecraft.registry.tag.TagEntry;
-import net.minecraft.registry.tag.TagGroupLoader;
+import net.minecraft.registry.tag.*;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -27,36 +25,27 @@ public class TagGroupLoaderMixin {
     )
     private <T> boolean exclusionsLib$removeEntry(TagEntry entry, TagEntry.ValueGetter<T> valueGetter,  Consumer<T> idConsumer, Operation<Boolean> original, TagEntry.ValueGetter<T> valueGetter1, List<TagGroupLoader.TrackedEntry> entries, @Local LocalRef<ImmutableSet.Builder<T>> builder)
     {
-        if(((TagEntryExclusionHolder)entry).exclusionsLib$isExcluded())
+        TagEntryExclusionHolder holder = (TagEntryExclusionHolder) entry;
+        if (!holder.exclusionsLib$isExcluded()) return original.call(entry, valueGetter, idConsumer);
+
+	    ExclusionsLib.LOGGER.info("[Exclusions Lib] The Following Tag has been detected: {}", entry);
+        List<T> list = new ArrayList<>(builder.get().build());
+        Identifier id = holder.exclusionsLib$getId();
+        boolean required = holder.exclusionsLib$isRequired();
+        if (holder.exclusionsLib$isTag())
         {
-            ExclusionsLib.LOGGER.info("[Exclusions Lib] The Following Tag has been detected: " + entry);
-            List<T> list = new java.util.ArrayList<>(builder.get().build().stream().toList());
-            Identifier id = ((TagEntryExclusionHolder)entry).exclusionsLib$getId();
-            boolean required = ((TagEntryExclusionHolder)entry).exclusionsLib$isRequired();
-            if(((TagEntryExclusionHolder)entry).exclusionsLib$isTag())
-            {
-                Collection<T> collection = valueGetter.tag(id);
-                if (collection == null) {
-                    return !required;
-                }
-                list.removeAll(collection);
-            }
-            else
-            {
-                T object = valueGetter.direct(id);
-                if (object == null) {
-                    return !required;
-                }
-                list.removeAll(Collections.singleton(object));
-            }
-            builder.set(new ImmutableSet.Builder<T>().addAll(list));
-            return true;
+            Collection<T> collection = valueGetter.tag(id);
+            if (collection == null) return !required;
+            list.removeAll(collection);
         }
         else
         {
-            return original.call(entry,valueGetter,idConsumer);
+            T object = valueGetter.direct(id);
+            if (object == null) return !required;
+            list.remove(object);
         }
+        builder.set(new ImmutableSet.Builder<T>().addAll(list));
+        return true;
     }
-
 
 }
