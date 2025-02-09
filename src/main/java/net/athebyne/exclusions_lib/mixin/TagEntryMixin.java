a6ec25773c5fcf3dc05f49bb1c357fabe1913628
imreallybadatnames™️ <68000899+imreallybadatnames@users.noreply.github.com>
2025-02-10 09:34:09 +1100

    random bullshit go (#2)

--- src/main/java/net/athebyne/exclusions_lib/mixin/TagEntryMixin.java
+++ src/main/java/net/athebyne/exclusions_lib/mixin/TagEntryMixin.java
@@ -1,19 +1,15 @@
 package net.athebyne.exclusions_lib.mixin;
 
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.datafixers.kinds.App;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.athebyne.exclusions_lib.extensions.TagEntryExclusionHolder;
 import net.minecraft.registry.tag.TagEntry;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.Objects;
 import java.util.function.Function;
@@ -34,23 +30,23 @@ public abstract class TagEntryMixin implements TagEntryExclusionHolder {
     @Unique
     private Boolean excluded;
 
-    @WrapOperation(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;create(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;"))
-    private static <O> Codec<O> wrap_create(
-            Function<RecordCodecBuilder.Instance<O>, ? extends App<RecordCodecBuilder.Mu<O>, O>> builder,
-            Operation<Codec<O>> original) {
-        return original.call(
-                TagEntryMixin.codecBuilder(builder));
+    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;create(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;"))
+    private static <O> Function<RecordCodecBuilder.Instance<O>, ? extends App<RecordCodecBuilder.Mu<O>, O>> wrap_create(
+            Function<RecordCodecBuilder.Instance<O>, ? extends App<RecordCodecBuilder.Mu<O>, O>> builder) {
+        return TagEntryMixin.codecBuilder(builder);
     }
+
     @Unique
     private static <O> Function<RecordCodecBuilder.Instance<O>, ? extends App<RecordCodecBuilder.Mu<O>, O>> codecBuilder(Function<RecordCodecBuilder.Instance<O>, ? extends App<RecordCodecBuilder.Mu<O>, O>> builder) {
         return instance -> instance.group(
-                RecordCodecBuilder.mapCodec(builder).forGetter(Function.identity()), Codec.BOOL.optionalFieldOf("excluded", false)
-                        .forGetter(exclusion -> (((TagEntryExclusionHolder) exclusion).exclusionsLib$isExcluded()))
+                RecordCodecBuilder.mapCodec(builder).forGetter(Function.identity()),
+                Codec.BOOL.optionalFieldOf("excluded", false).forGetter(exclusion -> (((TagEntryExclusionHolder) exclusion).exclusionsLib$isExcluded()))
         ).apply(instance, (exclusion, boolField) -> {
             ((TagEntryExclusionHolder) exclusion).exclusionsLib$setExcluded(boolField);
             return exclusion;
         });
     }
+
     @Override
     public Boolean exclusionsLib$isExcluded()
     {
@@ -68,17 +64,18 @@ public abstract class TagEntryMixin implements TagEntryExclusionHolder {
     {
         return this.tag;
     }
+
     @Override
     public boolean exclusionsLib$isRequired()
     {
         return this.required;
     }
-    @Override public Identifier exclusionsLib$getId()
+
+    @Override
+    public Identifier exclusionsLib$getId()
     {
         return this.id;
     }
-
-
 
 }
 
--- src/main/java/net/athebyne/exclusions_lib/predicates/OverlapsStructureBlockPredicate.java
+++ src/main/java/net/athebyne/exclusions_lib/predicates/OverlapsStructureBlockPredicate.java
@@ -1,81 +1,82 @@
 package net.athebyne.exclusions_lib.predicates;
 
-import com.mojang.serialization.Codec;
+import com.mojang.serialization.*;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryCodecs;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.registry.entry.RegistryEntryList;
-import net.minecraft.structure.StructurePiece;
-import net.minecraft.structure.StructureStart;
+import net.athebyne.exclusions_lib.ExclusionsLib;
+import net.minecraft.registry.*;
+import net.minecraft.registry.entry.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.structure.*;
 import net.minecraft.util.math.*;
 import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.blockpredicate.BlockPredicate;
-import net.minecraft.world.gen.blockpredicate.BlockPredicateType;
+import net.minecraft.world.gen.blockpredicate.*;
 import net.minecraft.world.gen.structure.Structure;
-import net.athebyne.exclusions_lib.*;
-import org.apache.commons.lang3.mutable.MutableBoolean;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-
 public class OverlapsStructureBlockPredicate implements BlockPredicate {
 
-    public static final Codec<OverlapsStructureBlockPredicate> CODEC = RecordCodecBuilder.create((instance) -> instance.group(Vec3i.createOffsetCodec(16).optionalFieldOf("offset", BlockPos.ORIGIN).forGetter((predicate) -> predicate.offset),
-            RegistryCodecs.entryList(RegistryKeys.STRUCTURE).optionalFieldOf("structures").forGetter(OverlapsStructureBlockPredicate::structure),
-            Codec.intRange(0, 32).optionalFieldOf("range", 0).forGetter((predicate) -> predicate.range)).apply(instance, OverlapsStructureBlockPredicate::new));
+    public static final MapCodec<OverlapsStructureBlockPredicate> CODEC = RecordCodecBuilder.mapCodec(
+            (instance) ->
+                    instance.group(
+                            Vec3i.createOffsetCodec(16).optionalFieldOf("offset", BlockPos.ORIGIN).forGetter((predicate) -> predicate.offset),
+                            RegistryCodecs.entryList(RegistryKeys.STRUCTURE).optionalFieldOf("structures").forGetter(OverlapsStructureBlockPredicate::structure),
+                            Codec.intRange(0, 32).optionalFieldOf("range", 0).forGetter((predicate) -> predicate.range)
+                    ).apply(instance, OverlapsStructureBlockPredicate::new)
+    );
 
     private final Vec3i offset;
     private final int range;
+    private final List<Structure> structures;
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private final Optional<RegistryEntryList<Structure>> rawStructures;
-    private final Optional<List<Structure>> structures;
-    public OverlapsStructureBlockPredicate(Vec3i offset,  Optional<RegistryEntryList<Structure>> structures, int range) {
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public OverlapsStructureBlockPredicate(Vec3i offset, Optional<RegistryEntryList<Structure>> structures, int range) {
         this.rawStructures = structures;
-        if (structures.isEmpty()) {
-            this.structures = Optional.empty();
-        } else {
-            this.structures = Optional.of(structures.get().stream().map(RegistryEntry::value).collect(Collectors.toList()));
-        }
+        this.structures = structures
+                .map(s -> s.stream().map(RegistryEntry::value).collect(Collectors.toList()))
+                .orElse(null);
         this.offset = offset;
         this.range = range;
 
     }
+    
     public boolean test(StructureWorldAccess structureWorldAccess, BlockPos blockPos) {
-        StructureAccessor accessor = structureWorldAccess.toServerWorld().getStructureAccessor();
+        ServerWorld world = structureWorldAccess.toServerWorld();
+        StructureAccessor accessor = world.getStructureAccessor();
         BlockPos blockPosOffset = blockPos.add(this.offset);
-        BlockBox exclusionZone = new BlockBox(blockPosOffset.getX() - this.range, blockPosOffset.getY() - this.range, blockPosOffset.getZ() - this.range,blockPosOffset.getX() + this.range, blockPosOffset.getY() + this.range, blockPosOffset.getZ() + this.range);
-        for(var struct : accessor.getStructureReferences(blockPosOffset).entrySet())
-        {
-            if(this.structures.isPresent() && !this.structures.get().contains(struct.getKey()))
-            {
-                continue;
-            }
-            Predicate<StructureStart> predicate = start -> {
-                for (StructurePiece piece : start.getChildren()) {
-                    if (piece.getBoundingBox().intersects(exclusionZone)) {
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            MutableBoolean overlappingBox = new MutableBoolean(false);
-            accessor.acceptStructureStarts(struct.getKey(), struct.getValue(), (start) -> {
-                if (overlappingBox.isFalse() && predicate.test(start)) {
-                    overlappingBox.setTrue();
-                }
-            });
-
-            if (overlappingBox.isTrue()) {
-                return true;
+        Predicate<StructureStart> predicate = makePredicate(blockPosOffset);
+        for (var struct : accessor.getStructureReferences(blockPosOffset).entrySet()) {
+            if (this.structures != null && !this.structures.contains(struct.getKey())) continue;
+            for (long pos : struct.getValue()) {
+                ChunkSectionPos sectionPos = ChunkSectionPos.from(new ChunkPos(pos), world.getBottomSectionCoord());
+                StructureStart start = accessor.getStructureStart(
+                        sectionPos, struct.getKey(), world.getChunk(sectionPos.getSectionX(), sectionPos.getSectionZ(), ChunkStatus.STRUCTURE_STARTS)
+                );
+                if (start != null && start.hasChildren() && predicate.test(start)) return true;
             }
         }
         return false;
+    }
+
+    private @NotNull Predicate<StructureStart> makePredicate(BlockPos blockPosOffset) {
+        final BlockBox exclusionZone = new BlockBox(
+                blockPosOffset.getX() - this.range, blockPosOffset.getY() - this.range, blockPosOffset.getZ() - this.range,
+                blockPosOffset.getX() + this.range, blockPosOffset.getY() + this.range, blockPosOffset.getZ() + this.range
+        );
+	    return start -> {
+	        for (StructurePiece piece : start.getChildren())
+	            if (piece.getBoundingBox().intersects(exclusionZone)) return true;
+	        return false;
+	    };
     }
 
     public Optional<RegistryEntryList<Structure>> structure() {
